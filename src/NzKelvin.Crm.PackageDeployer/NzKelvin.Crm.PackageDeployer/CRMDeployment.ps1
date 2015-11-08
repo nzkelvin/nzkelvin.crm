6528f3f4e8671e990e73ae2bf1e3e05a2cc99148
Kelvin Shen <kelvinshen@outlook.com>
2015-11-08 16:50:43 +1300

    NzKelvin.Crm.PackageDeployer

---
+++ src/NzKelvin.Crm.PackageDeployer/NzKelvin.Crm.PackageDeployer/CRMDeployment.ps1
@@ -0,0 +1,6 @@
+﻿$PackageDirectory = $pwd
+cd $PackageDirectory
+Add-PSSnapin Microsoft.Xrm.Tooling.Connector
+Add-PSSnapin Microsoft.Xrm.Tooling.PackageDeployment
+$CRMConn = Get-CrmConnection -DeploymentRegion Oceania -OnlineType Office365 -OrganizationName mrxrm -Credential $Cred
+Import-CrmPackage -CrmConnection $CRMConn -PackageDirectory $PackageDirectory -PackageName NzKelvin.Crm.PackageDeployer.dll -Verbose
