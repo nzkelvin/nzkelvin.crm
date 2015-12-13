8855b26c59c7e5eaa0795d65e4f55c8febc0261a
Kelvin Shen <kelvinshen@outlook.com>
2015-12-13 22:53:14 +1300

    stable version

--- src/NzKelvin.Crm.Client/NzKelvin.Crm.Client.Ribbon/AccountAppointmentAction.js
+++ src/NzKelvin.Crm.Client/NzKelvin.Crm.Client.Ribbon/AccountAppointmentAction.js
@@ -6,7 +6,10 @@
 
     var jqueryuicssUrl = rootUrl + 'jqueryui.min.css';
     var schedulerhtmlUrl = rootUrl + 'AccountAppointmentScheduler.html';
-    var jqueryuijsUrl = Xrm.Page.context.getClientUrl() + '/_static/_common/scripts/jquery_ui_1.8.21.min.js' //rootUrl + 'jqueryui.min.js';
+    var jqueryuijsUrl = 'jqueryui.min.js';
+    if (typeof Xrm != "undefined") {
+        jqueryuijsUrl = Xrm.Page.context.getClientUrl() + '/_static/_common/scripts/jquery_ui_1.8.21.min.js';
+    }
 
     //debugger;
     console.log(jqueryuicssUrl); //XXX
@@ -16,13 +19,6 @@
         href: jqueryuicssUrl
     }).appendTo('head');
 
-    //var url = null;
-    //if (typeof Xrm == "undefined") {
-    //    url = "AccountAppointmentScheduler.html";
-    //} else {
-    //    url = Xrm.Page.context.getClientUrl() + "//WebResources/ks_/RibbonDemo/AccountAppointmentScheduler.html";
-    //}
-
     console.log(schedulerhtmlUrl); //XXX
     $('<iframe/>', {
         id: 'appointment-scheduler',
@@ -31,18 +27,18 @@
     }).appendTo('body').hide();
 
     console.log(jqueryuijsUrl); //XXX
-    $.getScript(jqueryuijsUrl, function () {
-        $("#appointment-scheduler").show();
+    $.getScript(jqueryuijsUrl, function () { //??? Why has to getScript
+        //$("#appointment-scheduler").show();
 
         $("#appointment-scheduler").dialog({
-            height: 320,
-            modal: true,
-            buttons: {
-                Ok: function () {
-                    $(this).dialog("close");
-                    $("#appointment-scheduler").remove();
-                }
-            }
+            height: 260,
+            modal: true
+            //buttons: {
+            //    Ok: function () {
+            //        $(this).dialog("close");
+            //        $("#appointment-scheduler").remove();
+            //    }
+            //}
         });
     });
 
