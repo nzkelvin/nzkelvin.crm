d1bc514043c6d93dc35543c989f735226c12a2f9
Kelvin Shen <kelvinshen@outlook.com>
2016-01-27 17:31:37 +1300

    Dialog works in crm ribbon 1st time

---
+++ src/NzKelvin.Crm.ModalWindow/CrmVSSolution1/CrmPackage/WebResources/Script (JScript)/ks_NzKelvin.account.ribbon.js
@@ -0,0 +1,29 @@
+﻿function openSchedulerDialog() {
+    var dialogId = 'dialog';
+    var dialogTitle = 'Scheduler';
+    var dialogHeight = 300;
+    var dialogWidth = 350;
+    debugger;
+    var dialogContentSrc = 'ks_NzKelvin.account.Scheduler.html';
+    var jqueryuiCssSrc = 'ks_common/jqueryui/jquery_ui.css';
+    if (typeof (Xrm) !== typeof (undefined) && Xrm
+        && typeof (Xrm.Page) !== typeof (undefined) && Xrm.Page
+        && typeof (Xrm.Page.context) !== typeof (undefined) && Xrm.Page.context) {
+        var webResourcesUrl = Xrm.Page.context.getClientUrl() + '/WebResources/';
+        dialogContentSrc = webResourcesUrl + dialogContentSrc;
+        jqueryuiCssSrc = webResourcesUrl + jqueryuiCssSrc;
+    }
+    var modalDialog = true;
+
+    var dlg = new ks.Dialog(
+        dialogId,
+        dialogTitle,
+        dialogHeight,
+        dialogWidth,
+        dialogContentSrc,
+        jqueryuiCssSrc,
+        modalDialog
+    );
+
+    dlg.open();
+}
---
+++ src/NzKelvin.Crm.ModalWindow/CrmVSSolution1/CrmPackage/WebResources/Script (JScript)/ks_dialog/Dialog.js
@@ -0,0 +1,71 @@
+﻿if (typeof (ks) == typeof (undefined) || !ks) {
+    ks = { __namespace: true };
+}
+
+ks.Dialog = function (dialogId, dialogTitle, dialogHeight, dialogWidth, dialogContentSrc, jqueryuiCssSrc, modalDialog) {
+    this.dialogId = dialogId;
+    this.dialogTitle = dialogTitle;
+    this.dialogHeight = dialogHeight;
+    this.dialogWidth = dialogWidth;
+    this.dialogContentSrc = dialogContentSrc;
+    this.jqueryuiCssSrc = jqueryuiCssSrc;
+    this.modalDialog = typeof (modalDialog) === 'boolean' ? modalDialog : true;
+}
+
+ks.Dialog.prototype.open = function () {
+    this.init();
+    this.dialog.dialog('open');
+}
+
+ks.Dialog.prototype.init = function () {
+    // load jquery ui
+    var jqueryUiCssSource = this.jqueryuiCssSrc;//'./ks_common/jqueryui/jquery_ui.css';
+    var jqueryUiCssJqueryResults = $('link[href="' + jqueryUiCssSource + '"]');
+    var jqueryUiCss = null;
+    if (!jqueryUiCssJqueryResults.length) {
+        var jqueryUiCss = $('<link>', {
+            rel: 'stylesheet',
+            href: jqueryUiCssSource
+        });
+        
+        jqueryUiCss.appendTo('head');
+    } else {
+        jqueryUiCss = jqueryUiCssJqueryResults[0];
+    }
+
+    var dialog = $('#' + this.dialogId);
+    if (!dialog.length) {
+        dialog = $('<div>', {
+            id: this.dialogId,
+            title: this.dialogTitle
+        });
+    }
+    dialog.appendTo('body');
+    dialog.dialog({
+        autoOpen: false,
+        height: this.dialogHeight,
+        width: this.dialogWidth,
+        modal: this.modalDialog
+    });
+
+    // dialog content
+    dialogFrame = $('<iframe>', {
+        src: this.dialogContentSrc,
+        frameborder: 0,
+        seamless: 'seamless'
+    })
+    dialogFrame.css('width', '100%');
+    dialogFrame.css('height', '100%');
+    dialogFrame.appendTo(dialog);
+
+    // clean up in dialog close event
+    dialog.on('dialogclose', { dlg: dialog }, function (event) {
+        event.data.dlg.remove();
+    });
+
+    this.dialog = dialog;
+        
+    
+
+
+}
